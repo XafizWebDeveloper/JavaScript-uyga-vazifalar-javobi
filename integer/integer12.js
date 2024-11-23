8767363dede18c6f8bf4c2362b544b2c8b48ab96
XafizWebDeveloper <xafizrasulov1125@gmail.com>
2024-11-23 16:07:34 +0500

    integer12

---
+++ integer/integer12.js
@@ -0,0 +1,9 @@
+/* Berilgan ifodaning natijasini chiqaring. 2(n+3 )2 ( pow funksiyasidan foydalaning)
+Input : n .
+Output : natija   */
+
+let n = 12;
+
+let natija = 2*(Math.pow((n+3),2)); 
+
+console.log(natija); // 450
