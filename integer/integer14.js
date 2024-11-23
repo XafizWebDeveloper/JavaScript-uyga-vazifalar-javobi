ef3a018eb6141083399c427991e4943c2f5b66d1
XafizWebDeveloper <xafizrasulov1125@gmail.com>
2024-11-23 16:42:42 +0500

    int14

---
+++ integer/integer14.js
@@ -0,0 +1,10 @@
+/* Berilgan ifodaning natijasini chiqaring.( pow() funksiyasidan foydalaning)
+Input : n , x .
+Output : natija  */
+
+let n = 4;
+let x = 2;
+
+let natija = Math.pow(n,x) + Math.pow(6,x) 
+console.log(natija)  //52
+
