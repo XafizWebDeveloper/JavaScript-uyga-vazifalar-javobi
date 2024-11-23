0360eb60c3609de3817fa25ca8ebd3aed1420868
XafizWebDeveloper <xafizrasulov1125@gmail.com>
2024-11-23 17:06:35 +0500

    int17

---
+++ integer/integer16.js
@@ -0,0 +1,10 @@
+/* m sonni berilgan 2 xona birlikda yaxlitlang.
+( round ( ) funksiyasidan foydalaning)
+Input : m .
+Output : natija    */
+
+let m = 3.456;
+
+let natija = m.toFixed(2);
+
+console.log(natija)   // 3.46
---
+++ integer/integer17.js
@@ -0,0 +1,10 @@
+/* Berilgan ifodaning natijasini chiqaring. ( pow( ) funksiyasidan foydalaning ) 
+
+Input : x, y.
+Output : natija   */
+
+let x = 2;
+let y = 3;
+
+let natija = Math.pow(x,4) + 5*(Math.pow(x,2)) + Math.pow(x,3)*y  
+console.log(natija)       // 60
