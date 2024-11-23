3775f896a6917d7167180a9c22b6025311612542
XafizWebDeveloper <xafizrasulov1125@gmail.com>
2024-11-23 15:43:50 +0500

    JavaScript uyga vazifalar javobi

---
+++ integer/integer11.js
@@ -0,0 +1,16 @@
+//Ikki xonali butun son berilgan. Uning raqamlari yig'indisi va raqamlari ko'paytmasini toping.
+
+
+let ikkiXonaliSon = 24;
+
+let unliklarXonasidagiSon = (Math.floor(ikkiXonaliSon/10)); 
+
+let birlarXonasidagiSon = ikkiXonaliSon%10;
+
+//console.log(unliklarXonasidagiSon); 
+//console.log(birlarXonasidagiSon);   
+
+console.log(unliklarXonasidagiSon + birlarXonasidagiSon);
+console.log(unliklarXonasidagiSon * birlarXonasidagiSon);
+
+
