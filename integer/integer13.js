c875e3ca1ffa9e1f5c4ec7a4b013c86baaaf9826
XafizWebDeveloper <xafizrasulov1125@gmail.com>
2024-11-23 16:26:20 +0500

    int13

---
+++ integer/integer13.js
@@ -0,0 +1,10 @@
+/* Berilgan ifodaning natijasini chiqaring.( pow funksiyasidan foydalaning) 
+Input : n
+Output : natija   */
+
+let n = 2;
+
+let natija = Math.pow(((3+n)/2),2)
+
+console.log(natija);    // 6.25
+
