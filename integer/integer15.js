2ef96c39379dd4e4adc303ada479aa5d8d3eb3a1
XafizWebDeveloper <xafizrasulov1125@gmail.com>
2024-11-23 16:47:12 +0500

    int15

---
+++ integer/integer15.js
@@ -0,0 +1,10 @@
+/* Berilgan sonlarning (absolyut) qiymatini toping.
+( abs( ) funksiyasidan foydalaning )
+Input : a .
+Output : natija  */
+
+let a = -8;
+
+let natija = Math.abs(a)
+
+console.log(natija)  // 8
